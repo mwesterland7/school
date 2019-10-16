1d2effba606d1ee87575b67b60debb555ad48aa5
mwesterland <westersm9447@uwec.edu>
2019-10-16 15:59:24 -0500

    lab edits

--- biol 319/cockroach lab/cockroach.R
+++ biol 319/cockroach lab/cockroach.R
@@ -16,6 +16,7 @@ down <- filter(cockroach, Condition == "Down")
 up <- filter(cockroach, Condition == "Up")
 full <- filter(cockroach, Condition == "Full")
 half <- filter(cockroach, Condition == "Half")
+two <- filter(cockroach, Condition == "Two")
 
 fast_slow <- filter(cockroach, Condition == "Fast" | Condition == "Slow")
 full_half <- filter(cockroach, Condition == "Full" | Condition == "Half")
@@ -34,5 +35,7 @@ ggplot(data=full_half, aes(x=full_half$Condition, y=full_half$Spikes.Sec)) + sta
 ggplot(data=up_down, aes(x=up_down$Condition, y=up_down$Spikes.Sec)) + stat_summary(fun.y = mean, geom = "bar") + stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.3) + labs(x = "condition", y = "spikes per sec")
 
 ggplot(data=fast_slow, aes(x=fast_slow$Condition, y=fast_slow$Spikes.Sec)) + stat_summary(fun.y = mean, geom = "bar") + stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.3) + labs(x = "condition", y = "spikes per sec")
+
+ggplot(data=two, aes(x=two$Experiement, y=two$Spikes.Sec)) + stat_summary(fun.y = mean, geom = "bar") + stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.3) + labs(x = "condition", y = "spikes per sec")
 
 ggplot(data=cockroach, aes(x=cockroach$Condition, y=cockroach$Spikes.Sec)) + stat_summary(fun.y = mean, geom = "bar") + stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.3) + labs(x = "cockroach$Condition", y = "cockroach$Spikes.Sec")
