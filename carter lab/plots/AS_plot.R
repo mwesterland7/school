a132c03579ef02892890c5422bf41a41c6a1a10b
mwesterland <westersm9447@uwec.edu>
2019-10-23 10:27:10 -0500

    edits broski

--- carter lab/plots/AS_plot.R
+++ carter lab/plots/AS_plot.R
@@ -10,7 +10,7 @@ library('ggpubr')
 #        order = c(1,2,3,4,5,6,7,8,9,10,11),
 #        #order = c("Start-0:02:00", "0:02:00-0:04:00",	"0:04:00-0:06:00",	"0:06:00-0:08:00",	"0:08:00-0:10:00",	"0:10:00-0:12:00",	"0:12:00-0:14:00",	"0:14:00-0:16:00",	"0:16:00-0:18:00",	"0:18:00-0:20:00",	"0:20:00-0:22:00",	"0:22:00-0:24:00"),
 #        ylab = "Distance Traveled (mm)", xlab = "Time Intervals (min)"
-# )
+#)
 
 #state column names used for graph and axis titles
 plot(type = "o", col = "black", x = abby_data$time, y = abby_data$distance, xlab = "time", ylab ="distance")
