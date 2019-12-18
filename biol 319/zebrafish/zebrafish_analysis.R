bdd0605c3569ff840d7e9ed9bddfeb449f078f38
mwesterland <westersm9447@uwec.edu>
2019-12-17 22:28:54 -0600

    junior semester 1 finito!!

--- biol 319/zebrafish/zebrafish_analysis.R
+++ biol 319/zebrafish/zebrafish_analysis.R
@@ -2,21 +2,70 @@ install.packages('dplyr')
 install.packages('outliers')
 install.packages("ggpubr")
 install.packages('tidyverse')
+install.packages('plotrix')
 library('outliers')
 library('dplyr')
 library('ggpubr')
 library('tidyverse')
+library('plotrix')
 
 fish <- read.csv("as_data_2019.csv")
 
+control_bu$se <- std.error(control_bu$relative_bu)
+control_move$se <- std.error(control_move$relative_move)
+control_feed$se <- std.error(control_feed$relative_feed)
+
+diff_bu$se <- std.error(diff_bu$relative_bu)
+diff_move$se <- std.error(diff_move$relative_move)
+diff_feed$se <- std.error(diff_feed$relative_feed)
+
 #### tidying ####
-treatment_a <- filter(fish, fish$Treatment == "A")
-treatment_b <- filter(fish, fish$Treatment == "B")
-treatment_c <- filter(fish, fish$Treatment == "C")
-clean_fish <- na.omit(fish)
+treatment_a <- filter(fish, fish$treatment == "A")
+treatment_b <- filter(fish, fish$treatment == "B")
+treatment_c <- filter(fish, fish$teatment == "C")
+
+diff_bu <- filter(fish, fish$conditions == "DiffBU")
+diff_move <- filter(fish, fish$conditions == "DiffMove")
+diff_feed <- filter(fish, fish$conditions == "DiffFeed")
+
+control_bu <- filter(fish, fish$conditions == "ControlBU")
+control_move <- filter(fish, fish$conditions == "ControlM")
+control_feed <- filter(fish, fish$conditions == "ControlF")
+
+diff_bu$relative_bu <- diff_bu$movement
+diff_move$relative_move <- diff_move$movement
+diff_feed$relative_feed <- diff_feed$movement
+
+control_bu$relative_bu <- control_bu$movement
+control_move$relative_move <- control_move$movement
+control_feed$relative_feed <- control_feed$movement
+
+control_bu$control <- control_bu$treatment
+control_move$control <- control_move$treatment
+control_feed$control <- control_feed$treatment
 
 #### graphing ####
+ggplot(control_bu, aes(control, relative_bu)) +
+  stat_summary(geom = "bar", fun.y = mean, position = "dodge") +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge")
 
-geom_boxplot(treatment_a$DiffBU, treatment_b$DiffBU, treatment_c$DiffBU, aes(x=fish$Treatment, y=fish$di))
+ggplot(control_move, aes(control, relative_move)) +
+  stat_summary(geom = "bar", fun.y = mean, position = "dodge") +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge")
 
-geom_boxplot(mapping = aes(x=fish$Treatment, y=fish$DiffBU), treatment_a$DiffBU, treatment_b$DiffBU, treatment_c$DiffBU)
+ggplot(control_feed, aes(control, relative_feed)) +
+  stat_summary(geom = "bar", fun.y = mean, position = "dodge") +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge")
+
+ggplot(diff_bu, aes(treatment, relative_bu)) +
+  stat_summary(geom = "bar", fun.y = mean, position = "dodge") +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge")
+
+ggplot(diff_move, aes(treatment, relative_move)) +
+  stat_summary(geom = "bar", fun.y = mean, position = "dodge") +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge")
+
+ggplot(diff_feed, aes(treatment, relative_feed)) +
+  stat_summary(geom = "bar", fun.y = mean, position = "dodge") +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge")
+
